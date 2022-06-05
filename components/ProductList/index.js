324396637249187b1f650bc4f67f6186bcf6a3b4
Alejandro} <alejomayuri@gmail.com>
2022-06-04 23:21:20 -0500

    corrections

--- components/ProductList/index.js
+++ components/ProductList/index.js
@@ -57,8 +57,8 @@ export default function ProductList() {
                     <div className="filtersContainer">
                         <button className="closeFilters" onClick={() => setShowMenu(false)}>
                             <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M2 2L20 20" stroke="black" stroke-width="4" />
-                                <path d="M2 20L20 2" stroke="black" stroke-width="4" />
+                                <path d="M2 2L20 20" stroke="black" strokeWidth="4" />
+                                <path d="M2 20L20 2" stroke="black" strokeWidth="4" />
                             </svg>
                         </button>
                         <FilterCategory categories={categories} onChange={handleOnSelectCategory} />
